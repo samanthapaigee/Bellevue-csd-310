2695c2df0acef2e6b392c74446e3504521ca0e34
samanthapaigee <91444419+samanthapaigee@users.noreply.github.com>
2022-06-23 21:58:25 -0500

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,1 +1,9 @@
+from pymongo import MongoClient
 
+url = 'mongodb+srv://admin:admin@cluster0.jjyomii.mongodb.net/?retryWrites=true&w=majority'
+
+try:
+    conn = url
+    print ("success")
+except:
+    print("no")
