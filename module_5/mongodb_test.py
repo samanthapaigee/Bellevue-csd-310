f1fc51767d97671363a83e13330f3cc9b06a97b5
samanthapaigee <91444419+samanthapaigee@users.noreply.github.com>
2022-06-23 21:35:10 -0500

    Assignment 5.2

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -2,4 +2,4 @@ import pymongo
 url = "mongodb+srv://admin:admin@cluster0.jjyomii.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(url)
 db = client.pytech
-print(db.list_collection_names)
+print(db.list_collection_names) 
