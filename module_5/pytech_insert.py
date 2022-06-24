7e591c72dbd49d445a07243b20e2e130784c6673
samanthapaigee <91444419+samanthapaigee@users.noreply.github.com>
2022-06-23 23:47:17 -0500

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,9 +1,29 @@
 from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.jjyomii.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names())
 
-url = 'mongodb+srv://admin:admin@cluster0.jjyomii.mongodb.net/?retryWrites=true&w=majority'
+#defining names
+fred = {
+    "first_name": "Fred",
+    "last_name": "Weasely",
+    "student_id": "1007"    
+}
+george = {
+    "first_name": "George",
+    "last_name": "Weasely",
+    "student_id": "1008"   
+}
+ron = {
+    "first_name": "Ron",
+    "last_name": "Weasely",
+    "student_id": "1009"   
+}
 
-try:
-    conn = url
-    print ("success")
-except:
-    print("no")
+fred_student_id = db.student.insert_one(fred).inserted_id
+george_student_id = db.student.insert_one(george).inserted_id
+ron_student_id = db.student.insert_one(ron).inserted_id
+print(fred_student_id)
+print(george_student_id)
+print(ron_student_id)
