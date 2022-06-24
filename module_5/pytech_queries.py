d8de5393cc956175b389e86029964067ecbf6759
samanthapaigee <91444419+samanthapaigee@users.noreply.github.com>
2022-06-23 23:47:39 -0500

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,1 +1,8 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.jjyomii.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names())
 
+doc = db.student.find_one({"student_id":"1008"})
+print(doc["student_id"])
