2080e5049db9e3ab911e5ff7dc979197434f4f56
samanthapaigee <91444419+samanthapaigee@users.noreply.github.com>
2022-06-23 21:09:19 -0500

    Delete mongodb_test.py

--- mongodb_test.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-url = "mongodb+srv://admin:admin@cluster0.jjyomii.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(url)
-db = client.pytech
-print(db.list_collection_names)
