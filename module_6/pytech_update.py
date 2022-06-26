21c2421f3db65224273968eb89b1a3d14bda586c
samanthapaigee <91444419+samanthapaigee@users.noreply.github.com>
2022-06-26 18:59:20 -0500

    Update pytech_update.py

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -6,11 +6,15 @@ client = MongoClient(url)
 db = client.pytech
 col = db["student"]
 
+#print via find method :D
+for doc in col.find():
+    print(doc)
+
 #defining filter and new_last_name to use the variables to update the DB
 filter = {"student_id": "1007"}
 new_last_name = {"$set" : {"last_name": "Potter"}} 
 col.update_one(filter, new_last_name)
 
-#print :D
-for doc in col.find():
-    print(doc)
+#print updated line
+doc = db.student.find_one({"student_id":"1007"})
+print(doc)
