5974879ea0ef849f23240f38b44382b78bf35d5a
samanthapaigee <91444419+samanthapaigee@users.noreply.github.com>
2022-07-18 12:24:58 -0500

    Update FINAL_db_init.sql

--- whatabook/FINAL_db_init.sql
+++ whatabook/FINAL_db_init.sql
@@ -2,18 +2,18 @@
     Title: whatabook_db_init.sql
     Author: Samantha Lawrence
     Date: 18JUL2022
-    Description: Whatbook database initialization script
+    Description: whatbook database initialization script.
 */
 
 -- drop test user if exists 
 DROP USER IF EXISTS 'whatabook_user'@'localhost';
 
 
--- create pysports_user and grant them all privileges to the pysports database 
+-- create whatabook_user and grant them all privileges to the whatabook database 
 CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Password';
 
--- grant all privileges to the pysports database to user pysports_user on localhost 
-GRANT ALL PRIVILEGES ON pysports.* TO'whatabook_user'@'localhost';
+-- grant all privileges to the whatabook database to user whatabook_user on localhost 
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
 
 
 -- drop tables if they are present
@@ -21,7 +21,7 @@ DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS book;
 DROP TABLE IF EXISTS wishlist;
 DROP TABLE IF EXISTS store;
-GRANT ALL PRIVILEGES ON pysports.* TO'whatabook_user'@'localhost';
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
 
 -- create user table
 CREATE TABLE user (
@@ -62,6 +62,12 @@ CREATE TABLE store (
 INSERT INTO store(store_id, locale)
     VALUES('1', '4 Privet Dr, Little Whinging, Surrey');
 
+INSERT INTO store(store_id, locale)
+    VALUES('2', '12 Picket Post Close, Bracknell, Berkshire');
+
+INSERT INTO store(store_id, locale)
+    VALUES('3', '12 Grimmauld Place, Borough of Islington, London');
+
 -- insert book records
 INSERT INTO book(book_id, book_name, details, author)
     VALUES('1', 'Standard Books of Spells', 'The big book of any spell any witch or wizard will ever need!','Miranda Goshawk');
@@ -90,6 +96,15 @@ INSERT INTO book(book_id, book_name, details, author)
 INSERT INTO book(book_id, book_name, details, author)
     VALUES('9', 'The Wonder of Wigtown Wanderers, He Flew Like a Madman','I divide my time between my home in Nottinghamshire and where the Wigtown Wanderers are playing this week.','Kennilworthy Whisp');
 
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('10', 'Unfogging the Future','Because you probably should ... just not ... trust Trelawney','Cassandra Vablatsky');
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('11', 'The Philosophy of the Mundane: Why the Muggles Prefer Not to Know','The life story of the Dursley family','Professor Mordicus Egg');
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('12', 'Magical Drafts and Potions','Yup, Potter is still breaking into offices to steal potion ingredients.','Arsenic Jigger');
+
 -- insert user records
 INSERT INTO user(user_id, first_name, last_name)
     VALUES('1', 'Harry','Potter');
@@ -99,6 +114,14 @@ INSERT INTO user(user_id, first_name, last_name)
 
 INSERT INTO user(user_id, first_name, last_name)
     VALUES('3', 'Hermione','Granger');
+
+INSERT INTO user(user_id, first_name, last_name)
+    VALUES('4', 'Neville','Longbottom');
+
+INSERT INTO user(user_id, first_name, last_name)
+    VALUES('5', 'Luna','Lovegood');
+
+-- don't insert wishlist values, make this an ability in the program :)
 
 -- insert wishlist values
 INSERT INTO wishlist(wishlist_id, user_id, book_id)
